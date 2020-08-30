02752ffaf5ed37fa34983aa137c5b6654847de9a
joaoNoll <joaogabrielnoll@gmail.com>
2020-08-30 20:20:19 -0300

    .

---
+++ src/database/migrations/20200830230556-create-table-bands.js
@@ -0,0 +1,31 @@
+
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+
+    await queryInterface.createTable('brands', {
+      uid: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+
+    await queryInterface.dropTable('brands');
+  },
+};
